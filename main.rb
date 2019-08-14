abb4eddc94740f1ced9e1da6ba5d8b2be45829bd
jdm79 <james.malvern@gmail.com>
2019-08-14 13:45:54 +0100

    only adds journalism courses, uses nikkou library

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require 'nokogiri'
 require 'json'
 require 'httparty'
+require 'nikkou'
 
 def Scraper
 
@@ -10,27 +11,27 @@ def Scraper
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
 
-  parsed_page.css('div.induction-timetable-finder h2').map do |subject|
-    subjects << subject
-  end
-
-  subjects.each do |subject|
-      puts subject
+  parsed_page.css('div.induction-timetable-finder h2').text_includes('Journalism').map do |subject|
+    subjects << subject.text
   end
 
   url2 = "https://studenthub.city.ac.uk/new-students/induction-timetables?p=17"
   unparsed_page2 = HTTParty.get(url2)
   parsed_page2 = Nokogiri::HTML(unparsed_page2)
 
-  parsed_page2.css('div.induction-timetable-finder h2').map do |subject|
-    subjects << subject
+  # parsed_page2.search('div.induction-timetable-finder h2').xpath('text()').map do |subj|
+  #   subjects << subj
+  # end
+
+
+  parsed_page2.css('div.induction-timetable-finder h2').text_includes('Journalism').map do |subject|
+    subjects << subject.text
   end
 
   subjects.each do |subject|
       puts subject
   end
 
-  puts subjects.length
   # h2class = "induction-timetable-finder-result__title"
 end
 
