988181e952559366ed7539472ff079ee1dc9cbd3
jdm79 <james.malvern@gmail.com>
2019-08-14 15:53:51 +0100

    refactor: cleans it up a bit for now, different methods using global variables. make into a class next

--- main.rb
+++ main.rb
@@ -3,36 +3,34 @@ require 'json'
 require 'httparty'
 require 'nikkou'
 
+
+@subjects = []
+@url1 = "https://studenthub.city.ac.uk/new-students/induction-timetables"
+@url2 = "https://studenthub.city.ac.uk/new-students/induction-timetables?p=17"
+
 def Scraper
+  fetch(@url1)
+  fetch(@url2)
+  
+  printer
+end
 
-  subjects = []
-
-  url = "https://studenthub.city.ac.uk/new-students/induction-timetables"
+def fetch(url)
   unparsed_page = HTTParty.get(url)
   parsed_page = Nokogiri::HTML(unparsed_page)
 
   parsed_page.css('div.induction-timetable-finder h2').text_includes('Journalism').map do |subject|
-    subjects << subject.text
+    @subjects << subject.text
   end
+end
 
-  url2 = "https://studenthub.city.ac.uk/new-students/induction-timetables?p=17"
-  unparsed_page2 = HTTParty.get(url2)
-  parsed_page2 = Nokogiri::HTML(unparsed_page2)
-
-  # parsed_page2.search('div.induction-timetable-finder h2').xpath('text()').map do |subj|
-  #   subjects << subj
-  # end
-
-
-  parsed_page2.css('div.induction-timetable-finder h2').text_includes('Journalism').map do |subject|
-    subjects << subject.text
-  end
-
-  subjects.each do |subject|
+def printer
+  puts ""
+  puts "There are #{@subjects.length} journalism subjects listed"
+  puts "-------------------------------------------------------"
+  @subjects.each do |subject|
       puts subject
   end
-
-  # h2class = "induction-timetable-finder-result__title"
 end
 
 Scraper()
